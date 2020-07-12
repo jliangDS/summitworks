3aff0fdc84a3405b863aff072992b652b30d5ba2
Jun Liang <51464205+jliangDS@users.noreply.github.com>
2020-07-11 23:37:55 -0500

    Create ClassCstException1235.java

---
+++ ClassCstException1235.java
@@ -0,0 +1,19 @@
+package ExceptinDemo;
+
+ class ClassCstExceptin1235 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hello";
+Object o1=s; // widening
+
+Object o2=new Object();
+String s1=  (String) o2; // narrowing
+System.out.println("end of main");
+
+char c=123;
+
+
+	}
+
+}
