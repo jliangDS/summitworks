f5ca3a5dcd90686b00fea4cc515f26ae475677df
Jun Liang <51464205+jliangDS@users.noreply.github.com>
2020-07-11 23:34:42 -0500

    Create NullPointerDemo123.java

---
+++ NullPointerDemo123.java
@@ -0,0 +1,15 @@
+package ExceptinDemo;
+
+public class NullPointerDemo123 {
+	static String s="Maruthi";
+	static int i;// 0
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(i);
+		System.out.println(s.length());
+		
+		System.out.println(" last statement");
+	}
+
+}
